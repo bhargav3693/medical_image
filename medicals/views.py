c9ef65eb2863989fee510251ab39b4b21df015d5
bhargav3693 <bottabhargav7@gmail.com>
2026-03-31 23:21:34 +0530

    Suppress TensorFlow warnings in views.py
    Added environment variable to suppress TensorFlow warnings.

--- medicals/views.py
+++ medicals/views.py
@@ -1,3 +1,7 @@
+import os
+# MAGIC FIX: Idi TensorFlow warnings ni completely block chesthundi
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
 from django.shortcuts import render
 from users.forms import UserRegistrationForm
 
@@ -7,13 +11,9 @@ def index(request):
 def AdminLogin(request):
     return render(request, 'AdminLogin.html', {})
 
-
 def UserLogin(request):
     return render(request, 'UserLogin.html', {})
-
 
 def UserRegister(request):
     form = UserRegistrationForm()
     return render(request, 'UserRegistrations.html', {'form': form})
-
-
